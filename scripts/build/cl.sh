6cd8ab7c8e2b5eb65d70c5ae00f5d293da1d770f
Lucas Purkota <lucaspurkota@gmail.com>
2025-08-06 10:33:55 -0300

    fix

--- scripts/build/cl.sh
+++ scripts/build/cl.sh
@@ -3,10 +3,10 @@
 export GOOS=linux
 export GOARCH=amd64
 
-go build -o ../../bin/linux/auth_microservice ../cmd/auth_microservice/main.go
+go build -o ../../../bin/linux/auth_microservice ../../cmd/auth_microservice/main.go
 
 if [ $? -eq 0 ]; then
-  cp ../config/auth_microservice.conf ../../bin/linux/auth_microservice.conf
+  cp ../../config/auth_microservice.conf ../../../bin/linux/auth_microservice.conf
   echo "Build successful!"
 else
   echo "Build failed!"
--- scripts/build/cw.bat
+++ scripts/build/cw.bat
@@ -4,10 +4,10 @@ setlocal
 set GOOS=windows
 set GOARCH=amd64
 
-go build -o ../../bin/windows/auth_microservice.exe ../cmd/auth_microservice/main.go
+go build -o ../../../bin/windows/auth_microservice.exe ../../cmd/auth_microservice/main.go
 
 if %errorlevel% == 0 (
-    copy ../config/auth_microservice.conf ../../bin/windows/auth_microservice.conf
+    copy ../../configs/auth_microservice.conf ../../../bin/windows/auth_microservice.conf
     echo "Build successful!"
 ) else (
     echo "Build failed!"
